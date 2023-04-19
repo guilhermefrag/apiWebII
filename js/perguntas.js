794421b253ba3b88aa0ee17a7a8fdf173d1b7cb8
guilhermefrag <guilhermefragnani@icloud.com>
2023-04-19 10:54:48 -0300

    foi

--- js/perguntas.js
+++ js/perguntas.js
@@ -3,16 +3,32 @@ async function criarListaDeTestes() {
   const testes = await res.json();
   const urlParams = new URLSearchParams(window.location.search);
   const filter_teste = urlParams.get('teste');
-  const teste = testes.filter((t) => t.teste === filter_teste);
   const select = document.querySelector('#testes-select');
 
-  teste.map(teste => {
+  testes.map(teste => {
     const option = document.createElement('option');
     option.value = teste.teste;
     option.textContent = teste.teste;
+    option.defaultSelected = teste.teste === filter_teste;
     select.appendChild(option);
   });
+
+  select.addEventListener('change', changeTeste);  
+  await createListaDePerguntas();
+  
+  function changeTeste(){
+    const teste = select.value;
+    if (filter_teste !== teste) {
+      window.location.href = `/perguntas?teste=${teste}`;
+    }
+  }
+
+  async function createListaDePerguntas() {
+    
+  }
+
 }
+
 
 
 criarListaDeTestes();
